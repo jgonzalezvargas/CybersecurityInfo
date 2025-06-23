acdafee89c9468e4075de04b2e121344583ae910
jgonzalezvargas <jgonzalez.vargas@gmail.com>
2025-06-23 18:29:46 -0400

    cambio de planes, inclusion de bootstrap, ajuste del nav bar

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+  document.addEventListener("DOMContentLoaded", (event) => {
+
+const navbar = document.getElementById('mainNavbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
+
+  });
